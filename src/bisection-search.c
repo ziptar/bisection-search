03c8d007dff20a26d708552b8295110c2043177e
said <said@skynet.skynet.local>
2020-06-29 05:35:46 +0100

    Initial commit

---
+++ src/bisection-search.c
@@ -0,0 +1,57 @@
+/*
+ ============================================================================
+ Name        : bisection-search.c
+ Author      : Said TALEB ALI
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define LENGTH 10
+
+int main(void) {
+
+	int list[LENGTH] = {0 ,1 ,2 ,3 ,5 ,8 ,13 ,21 ,34 ,55};
+
+		int item = 0;
+		int a = 0;
+		int b = LENGTH - 1;
+		int middle  = 0;
+		bool found = false;
+
+		printf("Enter the number you are looking for: ");
+		scanf("%d", &item);
+
+		while(!found && a <= b)
+		{
+			middle  = (a + b) / 2;
+			if (item == list[middle])
+			{
+				found = true;
+			}
+			else if (item < list[middle])
+			{
+				b = middle - 1;
+			}
+			else
+			{
+				a = middle + 1;
+			}
+		}
+
+		if (!found)
+		{
+		        printf("The number %d is not found the list. \n", item);
+		}
+		else
+		{
+		        printf("The number %d is found at index %d in the list. \n", item, middle);
+		}
+
+	return EXIT_SUCCESS;
+}
